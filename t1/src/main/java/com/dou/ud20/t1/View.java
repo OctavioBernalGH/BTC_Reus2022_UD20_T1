15df23a400830b5a84ca3bb89b3ec5c8fb488fdf
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-05-03 10:15:41 +0200

    Se finaliza ejercicio_1

--- t1/src/main/java/com/dou/ud20/t1/View.java
+++ t1/src/main/java/com/dou/ud20/t1/View.java
@@ -26,6 +26,21 @@ public class View {
 	private JLabel lblNewLabel_1;
 	private JButton Cerrar;
 
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					View window = new View();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
 	public View() {
 		initialize();
 	}
