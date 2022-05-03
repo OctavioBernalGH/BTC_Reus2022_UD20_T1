34c92555bf861dc1528f71d786248ed702075568
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-05-03 09:26:51 +0200

    Estructura de proyecto.
    Se crea la estructura base del proyecto mediante MAVEN.

---
+++ t1/src/test/java/com/dou/ud20/t1/AppTest.java
@@ -0,0 +1,20 @@
+package com.dou.ud20.t1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
